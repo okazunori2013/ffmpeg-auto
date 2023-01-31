962ad76835847ecb77bb534c88795a1a5afb6dca
okazunori2013 <92250924+okazunori2013@users.noreply.github.com>
2023-01-31 15:43:03 +0900

    Update ffmpeg_extra.sh

--- settings/common/extra.sh/ffmpeg_extra.sh
+++ settings/common/extra.sh/ffmpeg_extra.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 _pre_configure(){
-    do_patch "https://raw.githubusercontent.com/okazunori2013/FFmpeg-Builds/master/patches/ffmpeg/master/0000-avformat-webvttdec-Ignore-REGION-and-STYLE-chunks.patch" am
+    do_patch "https://raw.githubusercontent.com/okazunori2013/FFmpeg-Builds/master/patches/ffmpeg/master/0001-avformat-webvttdec-Ignore-REGION-and-STYLE-chunks.patch" am
+    do_patch "https://raw.githubusercontent.com/okazunori2013/FFmpeg-Builds/master/patches/ffmpeg/master/0002-Support-for-HEVC-over-FLV.patch" am
 }
