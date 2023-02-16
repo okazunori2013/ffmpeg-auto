377add08350e04bc7905942bd63b619fed9d99e2
okazunotri2013 <92250924+okazunori2013@users.noreply.github.com>
2023-02-16 18:41:33 +0900

    remove gmp patch

--- patch/Patch_bash_scripts.bat
+++ patch/Patch_bash_scripts.bat
@@ -5,5 +5,4 @@ sed -i "/^color$/d" media-autobuild_suite/media-autobuild_suite.bat
 sed -i -r '/g (video^|audio) tools^|_check=\(xvc.pc^|sleep 5^|_check=\(libplacebo.^|# static do_vcs/i find $LOCALBUILDDIR -maxdepth 3 -type d -name "*-git" -exec rm -rf {} +' media-autobuild_suite/build/media-suite_compile.sh
 
 rem for fix build
-sed -i '2i pacman -U https://repo.msys2.org/mingw/mingw64/mingw-w64-x86_64-gmp-6.2.1-3-any.pkg.tar.zst --noconfirm --ask 20' media-autobuild_suite/build/media-suite_compile.sh
 sed -i  's!s:LIBPATH_lib(ass^|av(^|device^|filter))!s:LIBPATH_(lib)?(ffmpeg^|ass^|av^|archive^|bluray^|jpegxl(^|device^|filter))!g' media-autobuild_suite/build/media-suite_compile.sh
