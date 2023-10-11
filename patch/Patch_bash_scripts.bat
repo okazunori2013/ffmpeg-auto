362a51de33a0b355bb58adb3208a31c6e4136a23
okazunori2013 <92250924+okazunori2013@users.noreply.github.com>
2023-10-11 22:09:40 +0900

    compact

--- patch/Patch_bash_scripts.bat
+++ patch/Patch_bash_scripts.bat
@@ -3,8 +3,7 @@ sed -i 's/do_prompt /echo /g' media-autobuild_suite/build/media-suite_helper.sh 
 sed -i 's/read.*yn/yn=y/g' media-autobuild_suite/build/media-suite_update.sh
 sed -i "/^color$/d" media-autobuild_suite/media-autobuild_suite.bat
 sed -i -r '/g (video^|audio) tools^|_check=\(xvc.pc^|sleep 5^|_check=\(libplacebo.^|# static do_vcs/i find $LOCALBUILDDIR -maxdepth 3 -type d -name "*-git" -exec rm -rf {} +' media-autobuild_suite/build/media-suite_compile.sh
-sed -i '1i if exist media-autobuild_suite/local64 compact \/C \/S:media-autobuild_suite\/local64' media-autobuild_suite/media-autobuild_suite.bat
-sed -i '1i if exist media-autobuild_suite/local64 compact \/C \/S:media-autobuild_suite\/msys64' media-autobuild_suite/media-autobuild_suite.bat
+sed -i '1i if exist media-autobuild_suite/local64 compact \/C \/S:media-autobuild_suite' media-autobuild_suite/media-autobuild_suite.bat
 
 rem for fix build
 sed -i 's!s:LIBPATH_lib(ass^|av(^|device^|filter))!s:LIBPATH_(lib)?(ffmpeg^|ass^|av^|archive^|bluray^|jpegxl(^|device^|filter))!g' media-autobuild_suite/build/media-suite_compile.sh
